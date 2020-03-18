209103d3e3c11bdfec3e2d299b8e373479aa0f3b
Jonas Sharron desktop 202 <jfsharron@msn.com>
2020-03-18 12:03:47 -0700

    final: sharron_cuatomers, test_customers

--- sharron-sphere/sharron_customers.py
+++ sharron-sphere/sharron_customers.py
@@ -1,1 +1,55 @@
+"""
+File:   sharron_customers.py
+Resources to manage customer information.
+"""
+
+class Customer(object):
+    """Represents the customer"""
+
+    def __init__(self, name, address, phone):
+        """
+        Constructor to create a customer and contain their name, address, and
+        phone information.
+        """
+        self.name = name
+        self.address = address
+        self.phone = phone
+
+    def setAddress(self, address):
+        """
+        method for setting/resetting customer address information
+        """
+        self.address = address
+
+    def setPhone(self, phone):
+        """
+        method for setting/resetting customer phone information
+        """
+        self.phone = phone
+
+    def getName(self):
+        """
+        returns customer name
+        """
+        return self.name
+
+    def getAddress(self):
+        """
+        returns customer address
+        """
+        return self.address
+
+    def getPhone(self):
+        """
+        returns customer phone
+        """
+        return self.phone
+
+    def __str__(self):
+        """
+        returns the string representation of the customer
+        """
+        return "Name: " + self.name + \
+            "\nAddress: " + self.address + \
+            "\nPhone: " + self.phone
 
